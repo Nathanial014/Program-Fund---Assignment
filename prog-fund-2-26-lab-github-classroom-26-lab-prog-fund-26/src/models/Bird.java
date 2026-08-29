449847a6381c58f1ce5cc0e4c066d88d3c4a49a5
Nathanial <nathanialmcbarnett@gmail.com>
2026-08-29 16:14:29 +0100

    Added Owner and Bird Class

---
+++ prog-fund-2-26-lab-github-classroom-26-lab-prog-fund-26/src/models/Bird.java
@@ -0,0 +1,55 @@
+package models;
+
+public abstract class Bird extends Pet {
+
+    // fields //
+    private double wingSpan;
+    private boolean canFly;
+
+    // constructor //
+    public Bird(String name, int age, Owner owner, int id, double wingSpan, boolean canFly) {
+        super(name, age, owner, id);
+        setWingSpan(wingSpan);   // use setter for validation
+        this.canFly = canFly;
+    }
+
+    // getters and setters //
+    public double getWingSpan() {
+        return wingSpan;
+    }
+
+    public void setWingSpan(double wingSpan) {
+        if (wingSpan < 3 || wingSpan > 400) {
+            this.wingSpan = 3;   // default value
+        } else {
+            this.wingSpan = wingSpan;
+        }
+    }
+
+    public boolean isCanFly() {
+        return canFly;
+    }
+
+    public void setCanFly(boolean canFly) {
+        this.canFly = canFly;
+    }
+
+    // toString //
+    @Override
+    public String toString() {
+        return super.toString() +
+                ", wingspan=" + wingSpan +
+                "cm, canFly=" + canFly;
+    }
+
+    // equals //
+    @Override
+    public boolean equals(Object obj) {
+        if (!super.equals(obj)) return false;
+        if (!(obj instanceof Bird)) return false;
+
+        Bird other = (Bird) obj;
+        return this.wingSpan == other.wingSpan &&
+                this.canFly == other.canFly;
+    }
+}
